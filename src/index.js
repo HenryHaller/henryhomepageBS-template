82782213c9b84e63402987272b83d012a6ffd42a
Henry Haller <henrybhaller@gmail.com>
2019-06-20 13:53:51 -0400

    snapshot 6/20

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
 import 'bootstrap';
 import './styles/application.scss';
 
-const toggleExpansion = (event) => {
-	if ('#' + event.target.id !== event.data.collapse.collapse) {
-		console.log('mis matched collapse/collapser event');
+// const toggleExpansion = (event) => {
+// 	if ('#' + event.target.id !== event.data.collapse.collapse) {
+// 		console.log('mis matched collapse/collapser event');
+// 	} else {
+// 		document.querySelector(event.data.collapse.collapser).classList.toggle('expanded');
+// 	}
+// };
+
+// const collapses = [
+// 	{ collapse: '#skillsCollapse', collapser: '#skillsTitle' },
+// 	{ collapse: '#collapseOne', collapser: '#collapserOne' },
+// 	{ collapse: '#collapseTwo', collapser: '#collapserTwo' },
+// 	{ collapse: '#collapseThree', collapser: '#collapserThree' },
+// 	{ collapse: '#RSSHubCollapse', collapser: '#RSSHubController' },
+// 	{ collapse: '#PortfolioCollapse', collapser: '#PortfolioController' }
+// ];
+// collapses.forEach((collapse) => {
+// 	$(collapse.collapse).on('show.bs.collapse hide.bs.collapse', { collapse }, toggleExpansion);
+// });
+
+const toggleExpanded = (event) => {
+	// console.log(event.target, event.target.tagName);
+	if (event.target.tagName === 'H3' || event.target.tagName == 'H4') {
+		event.target.classList.toggle('expanded');
 	} else {
-		document.querySelector(event.data.collapse.collapser).classList.toggle('expanded');
+		event.target.querySelector('h3, h4').classList.toggle('expanded');
 	}
 };
 
-const collapses = [
-	{ collapse: '#skillsCollapse', collapser: '#skillsTitle' },
-	{ collapse: '#collapseOne', collapser: '#collapserOne' },
-	{ collapse: '#collapseTwo', collapser: '#collapserTwo' },
-	{ collapse: '#collapseThree', collapser: '#collapserThree' },
-	{ collapse: '#RSSHubCollapse', collapser: '#RSSHubController' },
-	{ collapse: '#PortfolioCollapse', collapser: '#PortfolioController' }
-];
-collapses.forEach((collapse) => {
-	$(collapse.collapse).on('show.bs.collapse hide.bs.collapse', { collapse }, toggleExpansion);
+document.querySelectorAll('.btn-link').forEach((expandable) => {
+	expandable.addEventListener('click', toggleExpanded);
 });
 
 //enable popovers
