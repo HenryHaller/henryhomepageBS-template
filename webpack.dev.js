5e3bc08806f3a6d5195c8065d6994db0472747b6
Henry Haller <henrybhaller@gmail.com>
2019-06-30 14:09:04 -0400

    source map and margin in accordion

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
 	mode: 'development',
-	devtool: 'none',
+	devtool: 'souce-map',
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
